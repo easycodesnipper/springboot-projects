ce6c1ccf90d51b3e05691a2daeefd58c773299ef
easycodesnipper <chen.dylan.chen@outlook.com>
2024-12-28 21:48:17 +0800

    Init commit

---
+++ springboot-auth-rbac/src/main/java/io/easycodesnipper/SpringbootAuthRbacApplication.java
@@ -0,0 +1,13 @@
+package io.easycodesnipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthRbacApplication.class, args);
+	}
+
+}
